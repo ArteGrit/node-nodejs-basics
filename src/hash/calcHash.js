87006883a06bcbae994bee563724b78881c4b01d
AreGrit <thistlefurcat@gmail.com>
2024-10-06 14:52:11 +0300

    feat: Add calculateHash js code

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { join } from "node:path";
+import { pipeline } from 'stream';
+
+// calcHash.js - implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt and logs it into
+// console as hex using Streams API
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = join(process.cwd(), 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const inputStream = createReadStream(filePath);
+
+  pipeline(
+    inputStream,
+    hash,
+    (err) => {
+      if (!err) {
+        console.log(`SHA256 hex hash: ${hash.digest('hex')}`);
+      }
+    }
+  );
 };
 
 await calculateHash();
