a6daee60ceff60e3056ea9c27381bfeda121bc75
AreGrit <thistlefurcat@gmail.com>
2024-10-06 15:13:48 +0300

    feat: Add streams/read js code

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'fs';
+import { join } from "node:path";
+
+// read.js - implement function that reads file fileToRead.txt content using Readable Stream and prints it's content
+// into process.stdout
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(process.cwd(), 'files', 'fileToRead.txt');
+
+  const stream = createReadStream(filePath);
+  stream.pipe(process.stdout);
 };
 
 await read();
