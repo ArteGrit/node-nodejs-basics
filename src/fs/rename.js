485a69a0ba5a2dc509e9ef236c81d17924d807c6
AreGrit <thistlefurcat@gmail.com>
2024-10-06 11:56:50 +0300

    feat: Add rename js code

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,33 @@
+import { rename as fsRename, access, constants } from 'node:fs';
+import { join } from 'path';
+
+// rename.js - implement function that renames file wrongFilename.txt to properFilename with extension .md (if there's
+// no file wrongFilename.txt or
+// properFilename.md already exists Error with message FS operation failed must be thrown)
+
+const ERROR_TEXT = 'FS operation failed';
+
 const rename = async () => {
-    // Write your code here 
+  const wrongFilename = join(process.cwd(), 'files', 'wrongFilename.txt');
+  const properFilename = join(process.cwd(), 'files', 'properFilename.md');
+
+  access(wrongFilename, constants.F_OK, (err) => {
+    if (err) {
+      throw new Error(`${ERROR_TEXT}: ${err.message}`);
+    }
+
+    access(properFilename, constants.F_OK, (err) => {
+      if (!err) {
+        throw new Error(`${ERROR_TEXT}: properFilename.md already exists`);
+      }
+
+      fsRename(wrongFilename, properFilename, (err) => {
+        if (err) {
+          throw new Error(`${ERROR_TEXT}: ${err.message}`);
+        }
+      });
+    });
+  });
 };
 
 await rename();
