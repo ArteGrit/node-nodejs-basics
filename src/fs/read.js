bc771ae999e23a457e7b428204306700c3d1c667
AreGrit <thistlefurcat@gmail.com>
2024-10-06 12:26:28 +0300

    feat: Add read js code

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+
+// read.js - implement function that prints content of the fileToRead.txt into console (if there's no file
+// fileToRead.txt Error with message FS operation failed must be thrown)
+
+import { join } from "node:path";
+import { promises as pfs } from "node:fs";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(process.cwd(), 'files', 'fileToRead.txt');
+
+  try {
+    await pfs.access(filePath);
+
+    const fileContent = await pfs.readFile(filePath, { encoding: 'utf8' });
+
+    console.log(fileContent);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
