75e8ad03347922a4ddc8954ba04496fc52265438
AreGrit <thistlefurcat@gmail.com>
2024-10-06 12:20:07 +0300

    feat: Add list js code

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { promises as pfs } from 'node:fs';
+import { join } from 'node:path';
+
+// list.js - implement function that prints all array of filenames from files folder into console (if files folder
+// doesn't exists Error with message FS operation failed must be thrown)
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = join(process.cwd(), 'files');
+
+  try {
+    await pfs.access(folderPath);
+
+    const files = await pfs.readdir(folderPath);
+
+    console.log(files);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
