7e4df91f021b6b5aaf1102f929a3262e9521bce7
AreGrit <thistlefurcat@gmail.com>
2024-10-06 12:59:20 +0300

    feat: Add env js code

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,22 @@
+
+// env.js - implement function that parses environment variables with prefix RSS_ and prints them to the console in the
+// format RSS_name1=value1; RSS_name2=value2
+
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';
+  const env = process.env;
+
+  if (env) {
+    const formatEnvString = Object.entries(env).reduce((str, [key, value]) => {
+      const formattedValue = key.startsWith(prefix) ? `${key}=${value}` : `${prefix}${key}=${value}`;
+        if (value) {
+          str = str ? `${str}; ${formattedValue}` : `${formattedValue}`;
+        }
+      return str;
+    }, '');
+
+    console.log(formatEnvString);
+  }
 };
 
 parseEnv();
