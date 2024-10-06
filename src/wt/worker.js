de863156d97fd15f09f9137866ab9d2651a0fb53
AreGrit <thistlefurcat@gmail.com>
2024-10-06 21:48:12 +0300

    feat: Add wt main.js, worker.js code

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,27 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort, isMainThread } from 'worker_threads';
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+// worker.js - extend given function to work with data received from main thread and implement function which sends
+// result of the computation to the main thread
+
+// Function to calculate the nth Fibonacci number
+const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
+
+// n should be received from main thread
+const sendResult = (n) => {
+  parentPort.postMessage({ status: 'resolved', data: n });
 };
 
-sendResult();
+const sendToMainThread = () => {
+  if (!isMainThread) {
+    parentPort.on('message', (n) => {
+      try {
+        const result = nthFibonacci(n);
+        sendResult(result);
+      } catch (error) {
+        parentPort.postMessage({ status: 'error', data: null });
+      }
+    });
+  }
+};
+
+sendToMainThread();
