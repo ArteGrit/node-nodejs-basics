76cec5ce11cbbe5b839ac6eda81904ec282b013e
AreGrit <thistlefurcat@gmail.com>
2024-10-07 17:12:03 +0300

    feat: Add cp.js code

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { fork } from 'child_process';
+import { join } from "path";
+
+/*
+cp.js - implement function spawnChildProcess that receives array of arguments args and creates child process from file
+script.js, passing these args to it. This function should create IPC-channel between stdin and stdout of master process
+ and child process:
+child process stdin should receive input from master process stdin
+child process stdout should send data to master process stdout
+ */
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const filePath = join(process.cwd(), 'files', 'script.js');
+
+  const child = fork(filePath, args, {
+    stdio: ['pipe', 'pipe', 'pipe', 'ipc'] // https://nodejs.org/api/child_process.html#optionsstdio https://nodejs.org/api/child_process.html#child_processforkmodulepath-args-options
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2']);
